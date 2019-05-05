a68c1f6b719448f492d1a7482438961795a8d0cf
TeluguGameboy <telugu.gameboy@gmail.com>
2019-05-04 21:07:56 -0700

    Finish formatting text on images somewhat cleanly.

--- 1_memes/main.py
+++ 1_memes/main.py
@@ -23,35 +23,34 @@ def get_rand_img():
 # Get text from computer microphone using Google Speech Recognition
 def get_txt_from_speech():
   initial_txt = SAMPLE_TEXT
-  txt = textwrap.fill(initial_txt, width=40)
+  txt = textwrap.fill(initial_txt, width=WIDTH/15)
   return txt
 
 # Add the text on the image and return final image
 def overlay_text_on_image(img, txt):
-  width, height = img.size
   draw = ImageDraw.Draw(img)
 
   # Draw black box at the bottom and add wrapped text
-  font = ImageFont.truetype(FONT_TYPE, int(height/20))
-  draw.rectangle(((0, height*(8/10)), (width, height*(19/20))), fill='black')
-  draw.text((width/10, height*(8/10)), txt, fill='white', font=font)
+  font = ImageFont.truetype(FONT_TYPE, int(HEIGHT/25))
+  draw.rectangle(((WIDTH/10, HEIGHT*(8/10)), (WIDTH*(9/10), HEIGHT*(19/20))), fill='black')
+  draw.text((WIDTH/10, HEIGHT*(8/10)), txt, fill='white', font=font)
 
-  img.save(IMAGE_URL)
-  return
+  return img
 
 if __name__ == '__main__':
   # First get the random image
   img = get_rand_img()
 
-  store_globals(img)
+  # Store global variables
+  WIDTH, HEIGHT = img.size
 
   # Second get the text from the user microphone
   txt = get_txt_from_speech()
 
   # Ensure that both the txt and img are valid
   if img != None and txt != None:
-    overlay_text_on_image(img, txt)
-    meme = Image.open(IMAGE_URL)
+    meme = overlay_text_on_image(img, txt)
+    meme.save(IMAGE_URL)
     meme.show()
   else:
     print('Error: Image or text not valid')
